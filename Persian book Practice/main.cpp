044edb098cbd33cb333cea5761b032ddd51816ff
Peyman Samandi <peyman@brightstardfw.com>
2017-11-04 13:49:37 -0500

    Initial Commit

---
+++ Persian book Practice/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Persian book Practice
+//
+//  Created by Peyman Samandi on 11/4/17.
+//  Copyright © 2017 Peyman Samandi. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
